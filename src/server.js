143dc2f77f9cbd4a8435ffa93cf5d4e1a67e6691
bleek42 <brandonleek42@gmail.com>
2020-04-28 13:26:59 -0400

    updated package.json again

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 /* eslint-disable strict */
 /* eslint-disable no-console */
 const app = require('./app');
-const { PORT, DATABASE_URL } = require('./config');
+const knex = require('knex');
+const { PORT, DATABASE_URL, NODE_ENV } = require('./config');
 
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL
 });
 
-// app.listen(PORT, () => console.log(`Server listening in ${NODE_ENV} mode at http://localhost:${PORT} and DB ${process.env.DB_URL} `));
+app.listen(PORT, () => console.log(`Server listening in ${NODE_ENV} mode at http://localhost:${PORT} and DB ${process.env.DATABASE_URL} `));
